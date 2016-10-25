1a9545bad0662b7f558516dffc1359bc0b08d3fe
Mandy van Zetten <mandy@competa.com>
2016-10-25 14:48:10 +0200

    Made The Mail Send

--- index.php
+++ index.php
@@ -53,26 +53,57 @@
   <div class="contact" id="contact-id">
     <h2>Contact</h2>
     <div class="contact-container">
-      <form action="">
-            <div class="form-group-container">
-              <label class="form-label" for="input-name">Name: </label>
-              <input type="text" class="form-input" id="input-name" placeholder="ex: John Doe">
-            </div>
-            <div class="form-group-container">
-              <label class="form-label" for="input-mail">Mail: </label>
-              <input type="text" class="form-input" id="input-mail" placeholder="ex: john@doe.com">
-            </div>
-            <div class="form-group-container">
-              <label class="form-label" for="input-subject">Subject: </label>
-              <input type="text" class="form-input" id="input-subject" placeholder="ex: Project X">
-            </div>
-            <div class="form-group-container">
-              <textarea id="input-message" class="form-textarea" placeholder="Message"></textarea>
-              <button class="form-submit">Send message</button>
-            </div>
-          </form>
+      <form name="contactform" method="post" action="">
+
+          <label class="form-label" for="input-mail">Mail: </label>
+          <input type="text" class="form-input" id="input-mail" name="email" maxlength="80" size="30" placeholder="ex: john@doe.com">
+
+
+          <textarea id="input-message" class="form-textarea" placeholder="Message" name="comments"></textarea>
+          <button type="submit" class="form-submit">Send message</button>
+
+      </form>
+      <?php
+
+  if(isset($_POST['email'])) {
+      $email_to = "mandy@competa.com";
+      $email_subject = "Website Email";
+      function died($error) {
+          echo $error;
+          die();
+      }
+      if (!filter_input(INPUT_POST, 'email', FILTER_VALIDATE_EMAIL) === true) {
+          died('enter a valid email');
+      }
+      $email_from = $_POST['email'];
+      $comments = $_POST['comments'];
+      $error_message = "";
+
+    if(strlen($comments) < 2) {
+      $error_message = 'The message you send does not contain an actual message!';
+    }
+    if(strlen($error_message) > 0) {
+      died($error_message);
+    }
+      $email_message = "Form details below.\n\n";
+      function clean_string($string) {
+        $bad = array("content-type","bcc:","to:","cc:","href");
+        return str_replace($bad,"",$string);
+      }
+      $email_message .= "Email: ".clean_string($email_from)."\n";
+      $email_message .= "Comments: ".clean_string($comments)."\n";
+
+      $headers = 'From: '.$email_from."\r\n".
+      'Reply-To: '.$email_from."\r\n" .
+      'X-Mailer: PHP/' . phpversion();
+      @mail($email_to, $email_subject, $email_message, $headers);
+      ?>
+        Email is send!
+      <?php
+      }
+      ?>
+
     </div>
   </div>
 </div>
-
 <?php include 'inc/footer.php'; ?>
