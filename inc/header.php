82ce75c749d5f9d3e20fe260da79de6a0008d945
Mandy <mandy@competa.com>
2017-07-19 15:00:47 +0200

    included font-awesome

--- inc/header.php
+++ inc/header.php
@@ -4,5 +4,6 @@
     <meta charset="utf-8">
     <title>Portfolio</title>
     <link rel="stylesheet" href="style.css" charset="utf-8">
+    <link rel="stylesheet" href="font-awesome/css/font-awesome.min.css">
   </head>
   <body>
---
+++ inc/link.php
@@ -0,0 +1,5 @@
+<div class="header_top">
+  <div class="header_top-img">
+    hi
+  </div>
+</div>
