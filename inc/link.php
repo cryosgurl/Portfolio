a69d5be85093bdcfc8188f09678c743b2db850ee
Mandy <mandy@competa.com>
2017-07-19 15:44:57 +0200

    added the links at the top

--- inc/link.php
+++ inc/link.php
@@ -1,5 +1,8 @@
 <div class="header_top">
   <div class="header_top-img">
-    hi
+    <a href="https://www.linkedin.com/in/cryosgurl/"><img class="link_img" src="img/linkedin.svg" alt="linkedin"></a>
+  </div>
+  <div class="header_top-img">
+    <a href="https://github.com/cryosgurl"><img class="link_img" src="img/github.svg" alt="github"></a>
   </div>
 </div>
--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php include 'inc/header.php'; ?>
 <?php include 'inc/link.php'; ?>
+<?php include 'inc/title.php'; ?>
