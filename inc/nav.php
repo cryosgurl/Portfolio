284ca5de17a9d48277c90e121c368d6f675afbb8
Mandy <mandy@competa.com>
2017-07-19 11:42:56 +0200

    changed to html

--- inc/nav.php
+++
@@ -1,8 +0,0 @@
-<div class="navigation">
-  <ul>
-    <li><a href="#default">Home</a></li>
-    <li><a href="#about-me">About</a></li>
-    <li><a href="#my-skills">Skills</a></li>
-    <li><a href="#contact-id">Contact</a></li>
-  </ul>
-</div>
