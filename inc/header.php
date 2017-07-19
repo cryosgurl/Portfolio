243ce26be3a810b8b3bdee42e9aae3b9a96854c2
Mandy <mandy@competa.com>
2017-07-19 11:26:37 +0200

    fresh start

--- inc/header.php
+++ inc/header.php
@@ -3,7 +3,6 @@
   <head>
     <meta charset="utf-8">
     <title>Portfolio</title>
-    <link rel="stylesheet" href="./css/normalize.css" charset="utf-8">
     <link rel="stylesheet" href="./css/portfolio.css" charset="utf-8">
   </head>
   <body>
