e2574ba81b528af4d53b54cf2c1a4d2f64449a19
Mandy <mandy@competa.com>
2017-07-20 11:54:16 +0200

    navigation styling, non working

---
+++ inc/nav.php
@@ -0,0 +1,21 @@
+<div class="nav_container">
+  <div class="nav_container-items">
+    <div class="nav_item">
+      <a href="#Home">HOME</a>
+    </div>
+    <div class="nav_item">
+      <a href="#about">ABOUT</a>
+    </div>
+
+    <div class="nav_image">
+      <img src="img/mandy.jpg" alt="Mandy">    
+    </div>
+
+    <div class="nav_item">
+      <a href="#skills">SKILLS</a>
+    </div>
+    <div class="nav_item">
+      <a href="#work">WORK</a>
+    </div>
+  </div>
+</div>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php include 'inc/header.php'; ?>
 <?php include 'inc/link.php'; ?>
 <?php include 'inc/title.php'; ?>
+<?php include 'inc/nav.php'; ?>
