6fe1969999ea43259e9f29943799be91c04b6483
Mandy <mandy@competa.com>
2017-07-21 14:31:52 +0200

    added a footer and all project text

--- inc/footer.php
+++ inc/footer.php
@@ -1,3 +1,12 @@
+
+    <div class="footer_bottom">
+      <div class="footer_bottom-img">
+        <a href="https://www.linkedin.com/in/cryosgurl/"><img class="link_img" src="img/linkedin.svg" alt="linkedin"></a>
+      </div>
+      <div class="footer_bottom-img">
+        <a href="https://github.com/cryosgurl"><img class="link_img" src="img/github.svg" alt="github"></a>
+      </div>
+    </div>
   <footer>
   </footer>
 </body>
--- inc/work.php
+++ inc/work.php
@@ -1,4 +1,6 @@
 <div class="work_container">
+
+
   <div class="work_container-item-left">
     <img src="img/mandy.jpg" alt="Mandy">
     <div class="arrow_right"></div>
@@ -13,10 +15,61 @@
   <div class="work_container-item-right">
     <div class="work_text">
       <h4>BRIGHTWAVE</h4>
-      <p>De afdeling HR/Field management binnen Brightwave werkt al enige tijd met een systeem in Excel om een headcount grafiek te genereren. Hierin wordt bijgehouden hoeveel Developers en system engineers van Brightwave op project zijn, op de "bank" zitten, trainee of stagiaire zijn. De data hiervan wordt in verschillende tabellen verzameld en vervolgens in 1 grafiek weergegeven.
+      <p>De afdeling HR/Field management binnen Brightwave werkt al enige tijd met een systeem in Excel om een headcount grafiek te genereren. Hierin wordt bijgehouden hoeveel Developers en system engineers van Brightwave op project zijn, op de "bank" zitten, trainee of stagiaire zijn. De data hiervan wordt in verschillende tabellen verzameld en vervolgens in 1 grafiek weergegeven.Om dit te automatiseren heb ik in een Scrum team van Developers een applicatie ontwikkeld met gebruik van MongoDB en NodeJS voor de Back-end en ExpressJS, PassportJS, AngularJS en D3JS voor de Front-end. Als Task runner is er gebruik gemaakt van GulpJS. Ik was voornamelijk verantwoordelijk voor de Front-end.
       </p>
     </div>
     <div class="arrow_left"></div>
     <img src="img/mandy.jpg" alt="Mandy">
   </div>
+
+<div class="work_container-item-left">
+  <img src="img/mandy.jpg" alt="Mandy">
+  <div class="arrow_right"></div>
+  <div class="work_text">
+    <h4>COMPETA WEBSITE</h4>
+    <p>Samen met een Scrum team heb ik gewerkt aan een vernieuwing van de website van Competa. Binnen deze
+website heb ik aan verschillende animaties gewerkt waarbij ik gebruik heb gemaakt van SVG, CSS(3) en JavaScript.
+Doordat we binnen de website Flexbox toegepast hebben en andere styling technieken die niet geheel browser compatible waren, heb ik verschillende fallbacks moeten implementeren. Hierdoor is de website volledig responsive
+en browser compatible opgeleverd.
+    </p>
+  </div>
+</div>
+
+<div class="work_container-item-right">
+  <div class="work_text">
+    <h4>COMPETA OFFICE DASHBOARD</h4>
+    <p>Voor de Back-office van Competa heb ik in samenwerking met verschillende Scrum teams gewerkt aan de ontwikkeling van een intern CRM systeem. Voor dit systeem, genaamd Office Dashboard, moest eerst een
+volledige architectuur bedacht worden. Voor de Back-end hebben wij gekozen voor het Laravel framework in combinatie met JavaScript en de Google API voor de login. Voor de Front-end hebben wij het systeem voorbereid
+om gebruik te kunnen maken van VueJS. Ik was binnen dit project verantwoordelijk voor het documenteren, implementeren van verschillende views met de daarbij behorende functionaliteiten zoals; het kunnen aanmaken, wijzigen en verwijderen persoonsgegevens.
+
+Dit project is nog in ontwikkeling, waarbij mijn werkzaamheden zullen veranderen naar Front-end.
+    </p>
+  </div>
+  <div class="arrow_left"></div>
+  <img src="img/mandy.jpg" alt="Mandy">
+</div>
+
+<div class="work_container-item-left">
+  <img src="img/mandy.jpg" alt="Mandy">
+  <div class="arrow_right"></div>
+  <div class="work_text">
+    <h4>FAIR TRADE SOFTWARE</h4>
+    <p>De website van de Fair Trade Software Foundation is flink verouderd en moest daarom in een nieuwe layout
+gegoten worden. De nieuwe website werd dusdanig ontworpen waarbij de functionaliteiten in eerste instantie onnatuurlijk voelde. Dit komt omdat het een horizontale website is waarbij de content verdeeld was over
+verschillende blok elementen. Elk blok had andere functionaliteiten zoals; video, audio, afbeeldingen, quotes en uitklapbare animaties. Hierbij was ik verantwoordelijk voor het opstellen van het grid waarbij ik gebruik heb gemaakt van Sass. Voor de verschillende animaties heb ik gebruik gemaakt van JavaScript. Om ervoor te zorgen dat het project geminified en verschillende bestanden samengevoegd worden, hebben we gebruik gemaakt voor de taskrunner Gulp.
+    </p>
+  </div>
+</div>
+
+<div class="work_container-item-right">
+  <div class="work_text">
+    <h4>COURSE REGISTRATION SYSTEM HAAGSE HOGESCHOOL</h4>
+    <p>Gezien de Haagse Hogeschool veel uitwisselingsstudenten heeft en zij hun vakken voor het volgende semester via papier werk invulde, hebben wij een applicatie ontwikkeld om dit te automatiseren. De Back-end (Java, TomCat) van deze applicatie werd door collegae in Kenia gebouwd en ik was samen met andere Scrum teams hier in Nederland verantwoordelijk voor de Front-end. Hierbij hebben wij gebruikt gemaakt van AngularJS om alles functioneel te kunnen maken via de API die opgesteld is door de Back-end. Daarnaast voor de styling hebben wij gebruik gemaakt van Sass.
+    </p>
+  </div>
+  <div class="arrow_left"></div>
+  <img src="img/mandy.jpg" alt="Mandy">
+</div>
+
+
 </div>
