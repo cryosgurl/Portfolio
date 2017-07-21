2be13d1895edd96ca074986d2e358e14062b768f
Mandy <mandy@competa.com>
2017-07-21 13:43:27 +0200

    Recent projects done

--- inc/work.php
+++ inc/work.php
@@ -1,11 +1,21 @@
 <div class="work_container">
   <div class="work_container-item-left">
     <img src="img/mandy.jpg" alt="Mandy">
-    <div class="arrow-right"></div>
+    <div class="arrow_right"></div>
+    <div class="work_text">
+      <h4>LORUM IPSUM</h4>
+      <p>  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer        tincidunt.
+      </p>
+    </div>
   </div>
-  
+
   <div class="work_container-item-right">
+    <div class="work_text">
+      <h4>LORUM IPSUM</h4>
+      <p>  Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer        tincidunt.
+      </p>
+    </div>
+    <div class="arrow_left"></div>
     <img src="img/mandy.jpg" alt="Mandy">
-    <div class="arrow-left"></div>
   </div>
 </div>
