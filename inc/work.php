092d2b7bea4f19dcb8c389225d2d3d083bb6ea91
Mandy <mandy@competa.com>
2017-07-21 12:00:15 +0200

    working on recent projects

--- inc/work.php
+++ inc/work.php
@@ -1,8 +1,11 @@
 <div class="work_container">
-  <div class="work_container-item-right">
-
-  </div>
   <div class="work_container-item-left">
-
+    <img src="img/mandy.jpg" alt="Mandy">
+    <div class="arrow-right"></div>
+  </div>
+  
+  <div class="work_container-item-right">
+    <img src="img/mandy.jpg" alt="Mandy">
+    <div class="arrow-left"></div>
   </div>
 </div>
